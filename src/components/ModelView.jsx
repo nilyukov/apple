f3f50f83bd48526a5ca42f84c65824e1deb94fcf
Nikita Ilyukov <nilyukov@gmail.com>
2024-03-22 14:11:03 +0300

    feat(Model): 3D Model by three and gsap

---
+++ src/components/ModelView.jsx
@@ -0,0 +1,46 @@
+import { OrbitControls, PerspectiveCamera, View } from '@react-three/drei';
+
+import * as THREE from 'three';
+import Lights from './Lights';
+import Loader from './Loader';
+import IPhone from './IPhone';
+import { Suspense } from 'react';
+
+const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, size, item }) => {
+    return (
+        <View
+            index={index}
+            id={gsapType}
+            className={`absolute size-full ${index === 2 ? '-right-full' : ''}`}>
+            {/* eslint-disable-next-line */}
+            <ambientLight intensity={0.3} />
+
+            <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+            <Lights />
+
+            <OrbitControls
+                makeDefault
+                ref={controlRef}
+                enableZoom={false}
+                enablePan={false}
+                rotateSpeed={0.4}
+                target={new THREE.Vector3(0, 0, 0)}
+                onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+            />
+
+            {/* eslint-disable-next-line */}
+            <group ref={groupRef} name={`${index === 1} ? 'small' : 'large`} position={[0, 0, 0]}>
+                <Suspense fallback={<Loader />}>
+                    <IPhone
+                        scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+                        item={item}
+                        size={size}
+                    />
+                </Suspense>
+            </group>
+        </View>
+    );
+};
+
+export default ModelView;
